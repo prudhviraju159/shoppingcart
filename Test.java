008d8394694b7433a45173a216ca72289cc03c5f
prudhviraju159 <peddiprudhvi159@gmail.com>
2016-07-20 21:47:08 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,19 @@
+package com.niit.shoppingcart;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test {
+	
+	public static void main(String[] args){
+
+		AnnotationConfigApplicationContext context= new AnnotationConfigApplicationContext();
+		
+		context.scan("com.niit.shoppingcart");
+		context.refresh();
+		
+		context.getBean("product");
+		
+		
+		System.out.println("successfully is created");
+	}		
+}
